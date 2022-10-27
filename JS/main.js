387e0eb06fa7a9d9e292ed792dbf801c570bd9fd
obielen91 <oliwiabielen91@gmail.com>
2022-10-27 20:39:37 +0200

    zadanie 29

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 3;
+let secondNumber = 9;
+
+function suma(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+
+let wynikDodawania = suma(firstNumber, secondNumber);
+console.log(wynikDodawania);
